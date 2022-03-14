88f547c80588d288a0d04622d920401e302eca8f
Lucas Munhoz <lucasmunhozarruda@gmail.com>
2022-03-14 18:04:30 -0300

    Servidor MovieCRUD

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { app } from './app';
+import { routes } from './routes';
+
+app.use(routes);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server UP on: http://localhost:${PORT}/`);
+});
